59d55be3391a166dd7a5b520f2a35c85bb85a35b
aakashDeveloper <ahanda205@gmail.com>
2020-04-30 02:20:29 +0100

    a

--- fullStack/src/routes/cityRoutes.js
+++ fullStack/src/routes/cityRoutes.js
@@ -1,54 +1,6 @@
 var express = require('express');
 var cityRouter = express.Router();
 
-var city = [
-    {
-		"id": 1,
-		"name": "Delhi",
-		"country_id": 1,
-		"country_name": "India",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_1.png",
-	},
-    {
-		"id": 3,
-		"name": "Mumbai",
-		"country_id": 1,
-		"country_name": "India",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_1.png",
-		
-    },
-    {
-		"id": 280,
-		"name": "New York City, NY",
-		"country_id": 216,
-		"country_name": "United States",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_216.png",
-		
-    },
-    {
-        "id": 32,
-		"name": "Pune",
-		"country_id": 1,
-		"country_name": "India",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_1.png",
-	        
-	},
-	{
-		"id": 77,
-		"name": "Chandigrah",
-		"country_id": 1,
-		"country_name": "India",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_1.png",
-	
-	},
-    {
-		"id": 4,
-		"name": "Bangalore",
-		"country_id": 1,
-		"country_name": "India",
-		"country_flag_url": "https://b.zmtcdn.com/images/countries/flags/country_1.png",
-    }
-]
 
 function router(menu){
 
